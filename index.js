1e8ab7e45c9ece2c4e51a0690d51cc2b979123b0
KrisVos130 <krisvos130@gmail.com>
2017-11-08 18:10:31 +0100

    Added cors and replaced express.static.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const config = require("config");
 
-app.use(express.static('locales'));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+const localesDir = __dirname + "/locales/";
+app.get("/*", (req, res) => {
+	let path = req.path;
+	if (path.indexOf('..') !== -1) return res.end();
+	res.set('Cache-Control', 'max-age=8640000000');
+
+	fs.access(localesDir + path, function (err) {
+		if (!err) {
+			res.sendFile(localesDir + path);
+		} else {
+			res.end();
+		}
+	});
+});
 
 app.listen(config.get('port'));
